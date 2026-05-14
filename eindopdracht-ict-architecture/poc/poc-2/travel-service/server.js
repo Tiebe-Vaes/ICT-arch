94144df5fa78d0d50c8aebe140ac4899a22c5eeb
Jamyang <Tenzinjamyang0901@gmail.com>
2026-05-14 18:20:20 +0200

    Create travel service where u can get trip data; Integrate redis client into travel service

---
+++ eindopdracht-ict-architecture/poc/poc-2/travel-service/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const redis = require("redis");
+
+const app = express();
+
+const client = redis.createClient({
+  url: "redis://redis:6379",
+});
+
+client.connect();
+
+app.get("/trip/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const cached = await client.get(id);
+
+  if (cached) {
+    console.log("Cache hit");
+
+    return res.json(JSON.parse(cached));
+  }
+
+  console.log("Cache miss");
+
+  const response = await fetch(`http://mock-api:3000/trip/${id}`);
+
+  const data = await response.json();
+
+  await client.set(id, JSON.stringify(data));
+
+  res.json(data);
+});
+
+app.listen(8080);
